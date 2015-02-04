3b63fc3771ba6dd4fe1ad975d27addf7546843c9
stephaniejn <stephaniejnilles@gmail.com>
2015-02-04 11:38:34 -0800

    Added Modal

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 /* put js code here */
 
-var redditApp = angular.module("RedditApp",[])
+var redditApp = angular.module("RedditApp",['ui.bootstrap'])
 
-redditApp.controller('SearchController', ['$scope', '$http', function($scope, $http){
+redditApp.controller('SearchController', ['$scope', '$http', '$modal', function($scope, $http, $modal){
     console.log('this file is running.');
     $scope.searchTerm = ""
     $scope.searches = [];
@@ -14,11 +14,8 @@ redditApp.controller('SearchController', ['$scope', '$http', function($scope, $h
             url: 'http://www.reddit.com/search.json',
             params: {
                 q: $scope.searchTerm
-                // type: $scope.searchType
             }
         }
-        // console.log(req)
-        // console.log($scope.searchTerm)
 
         $http(req).success(function(data){
             if($scope.searches.indexOf($scope.searchTerm) == -1){
@@ -26,33 +23,37 @@ redditApp.controller('SearchController', ['$scope', '$http', function($scope, $h
             };
             $scope.loading = false
             $scope.posts = data.data.children
-            // console.log($scope.posts[0].data.thumbnail)
 
         })
     }
         $scope.showImage = function () {
-
-    if ($scope.posts.data.thumbnail != "") {
-        // console.log($scope.posts.data.thumbnail)
-        return true;
+            // for (var i = 0, i<20, i++)
+            if(data.data.children[i].data.thumbnail.length >5){
+               return true
+            }
     }
-    };
 
     // NOT removing correct one
    $scope.remove = function(index) {
 
     var index = $scope.searches.indexOf($scope.searchTerm)
     console.log(index)
-    // console.log($scope.searches)
-    // console.log(index)
      $scope.searches.splice(index, 1);
     }
 
-}])
- // $scope.activeClass = function(search){
-    //     if ($scope.searches.indexOf($scope.searchTerm) != -1){
-    //           console.log($scope.searches.indexOf($scope.searchTerm))
-    //           return true
-    //     }
-    // }
+    $scope.showAbout = function(){
+        var aboutModal = $modal.open({
+            templateUrl:'aboutModal.html',
+            controller:'AboutModalCtrl',
+            size: 'md'
+        });
+    }
 
+}])
+
+redditApp.controller('AboutModalCtrl', ['$scope', '$modalInstance', function($scope, $modalInstance){
+    $scope.greeting= "Hi"
+    $scope.closeThisModal = function(){
+        $modalInstance.close();
+    }
+}])

4136f15bb10810d74e58357e04c65f9b6eaafd28
stephaniejn <stephaniejnilles@gmail.com>
2015-02-04 10:09:58 -0800

    Updated Homework

--- js/app.js
+++ js/app.js
@@ -1,3 +1,58 @@
 /* put js code here */
 
-console.log('this file is running.');
+var redditApp = angular.module("RedditApp",[])
+
+redditApp.controller('SearchController', ['$scope', '$http', function($scope, $http){
+    console.log('this file is running.');
+    $scope.searchTerm = ""
+    $scope.searches = [];
+    $scope.posts = {};
+
+    $scope.search = function(){
+
+        var req = {
+            url: 'http://www.reddit.com/search.json',
+            params: {
+                q: $scope.searchTerm
+                // type: $scope.searchType
+            }
+        }
+        // console.log(req)
+        // console.log($scope.searchTerm)
+
+        $http(req).success(function(data){
+            if($scope.searches.indexOf($scope.searchTerm) == -1){
+            $scope.searches.push({active:true, name:$scope.searchTerm})
+            };
+            $scope.loading = false
+            $scope.posts = data.data.children
+            // console.log($scope.posts[0].data.thumbnail)
+
+        })
+    }
+        $scope.showImage = function () {
+
+    if ($scope.posts.data.thumbnail != "") {
+        // console.log($scope.posts.data.thumbnail)
+        return true;
+    }
+    };
+
+    // NOT removing correct one
+   $scope.remove = function(index) {
+
+    var index = $scope.searches.indexOf($scope.searchTerm)
+    console.log(index)
+    // console.log($scope.searches)
+    // console.log(index)
+     $scope.searches.splice(index, 1);
+    }
+
+}])
+ // $scope.activeClass = function(search){
+    //     if ($scope.searches.indexOf($scope.searchTerm) != -1){
+    //           console.log($scope.searches.indexOf($scope.searchTerm))
+    //           return true
+    //     }
+    // }
+
